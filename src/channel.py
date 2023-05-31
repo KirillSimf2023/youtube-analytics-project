01334c846d8cfaa5b43705067fd3b345130162b3
kirill <lobbi.kirill@mail.ru>
2023-06-01 01:01:53 +0300

    Модифицирован конструктор Channel, чтобы после инициализации экземпляр имел следующие атрибуты, заполненные реальными данными канала: id канала название канала описание канала ссылка на канал количество подписчиков количество видео общее количество просмотров

--- src/channel.py
+++ src/channel.py
@@ -6,15 +6,27 @@ import isodate
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key = None
-    youtube = None
+    __api_key = os.getenv('YouTube_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=__api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала.
         Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.api_key = os.getenv('YouTube_API_KEY')
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.__channel_id = channel_id
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(channel['items'][0]['statistics']['viewCount'])
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+
+
 
 
     def print_info(self) -> None:
